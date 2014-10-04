45b45cc200f68ad96175006baab89b295541d77c
Octave <octave.auger@gmail.com>
2014-10-04 16:44:33 +0100

    changing heroku domain name for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Unicorn::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { host: 'http://stormy-inlet-9142.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://tranquil-wave-1154.herokuapp.com/' }
 end
