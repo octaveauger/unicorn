1a364f18c2ba31f3abd9f53e2f15fc0f7d02fda8
Octave <octave.auger@gmail.com>
2014-12-06 15:13:51 +0000

    corrected mispelling in production environment config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Unicorn::Application.configure do
     address:                 'smtp.gmail.com',
     port:                     587,
     user_name:                ENV['GMAIL_USERNAME'],
-    password:                 ENV['GMAIL_USERNAME'],
+    password:                 ENV['GMAIL_PASSWORD'],
     authentication:          'plain',
     enable_starttls_auto:    true
   }
